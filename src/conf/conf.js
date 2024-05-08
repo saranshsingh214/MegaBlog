bf4de4fbcde2720819d57a3f579159aac211cd4c
Saransh Singh <saranshsingh.it2021@nsec.ac.in>
2024-05-08 13:30:54 +0530

    Authentication Services

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,7 @@ const conf = {
     appwriteProjectId : String (import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId : String (import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId : String (import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBuckeId : String (import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appwriteBucketId : String (import.meta.env.VITE_APPWRITE_BUCKET_ID),
 
 }
 
