32ca63e1206d88243e9a0fe3621693affdae2785
Saransh Singh <saranshsingh.it2021@nsec.ac.in>
2024-06-26 19:45:00 +0530

    fixing errors

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ import { AuthLayout, Login } from './components/index.js'
 
 
 import AddPost from "./pages/AddPost";
-import Signup from './pages/Signup';
+import Signup from '@pages/Signup';
+
 import EditPost from "./pages/EditPost";
 
 import Post from "./pages/Post";
